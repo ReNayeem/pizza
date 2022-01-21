2a024560a4be8a8be43f719bbc413cd506f79f08
ReNayeem <renayeem@gmail.com>
2022-01-21 16:44:57 +0600

    custom scroll & nav

---
+++ script.js
@@ -0,0 +1,20 @@
+// scrollbar nav
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    } else {
+        nav.classList.remove("scroll.on");
+    }
+}
+// scrollbar nav end
+
+//responsive nav hide after clicked
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    })
+})
+//responsive nav hide after clicked end
